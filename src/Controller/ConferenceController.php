120feff275ec39fe5269235259a756c7a2b1e081
Luis Angel Agudelo Pajoy <angelus628@hotmail.com>
2022-04-11 21:47:52 +0200

    wip: comment paginator

---
+++ src/Controller/ConferenceController.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Entity\Conference;
+use App\Repository\CommentRepository;
+use App\Repository\ConferenceRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
+
+class ConferenceController extends AbstractController
+{
+    private Environment $environment;
+    private ConferenceRepository $conferenceRepository;
+    private CommentRepository $commentRepository;
+
+    public function __construct(
+        Environment $environment,
+        ConferenceRepository $conferenceRepository,
+        CommentRepository $commentRepository
+    ) {
+        $this->environment = $environment;
+        $this->conferenceRepository = $conferenceRepository;
+        $this->commentRepository = $commentRepository;
+    }
+
+    /**
+     * @throws RuntimeError
+     * @throws SyntaxError
+     * @throws LoaderError
+     */
+    #[Route('/', name: 'homepage')]
+    public function index(): Response
+    {
+        return new Response($this->environment->render('conference/index.html.twig', [
+            'conferences' => $this->conferenceRepository->findAll()
+        ]));
+    }
+
+    /**
+     * @throws RuntimeError
+     * @throws SyntaxError
+     * @throws LoaderError
+     */
+    #[Route('/conference/{id}', name: 'conference')]
+    public function show(Request $request, Conference $conference): Response
+    {
+        $offset = max(0, $request->query->get('offset', 0));
+        $paginator = $this->commentRepository->getCommentPaginator($conference, $offset);
+
+        return new Response($this->environment->render('conference/show.html.twig', [
+            'conference' => $conference,
+            'comments' => $paginator,
+            'previous' => $offset - CommentRepository::PAGINATOR_PER_PAGE,
+            'next' => min($paginator->count(), $offset + CommentRepository::PAGINATOR_PER_PAGE),
+        ]));
+    }
+}
---
+++ src/Entity/Comment.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CommentRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CommentRepository::class)]
+class Comment
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private int $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private string $author;
+
+    #[ORM\Column(type: 'text')]
+    private string $text;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private string $email;
+
+    #[ORM\Column(type: 'datetime_immutable')]
+    private \DateTimeImmutable $created_at;
+
+    #[ORM\ManyToOne(targetEntity: Conference::class, inversedBy: 'comments')]
+    #[ORM\JoinColumn(nullable: false)]
+    private Conference $conference;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private ?string $photoFilename = null;
+
+    public function __toString(): string
+    {
+        return sprintf('%s', $this->email);
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getAuthor(): ?string
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(string $author): self
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    public function getText(): ?string
+    {
+        return $this->text;
+    }
+
+    public function setText(string $text): self
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $created_at): self
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getConference(): ?Conference
+    {
+        return $this->conference;
+    }
+
+    public function setConference(?Conference $conference): self
+    {
+        $this->conference = $conference;
+
+        return $this;
+    }
+
+    public function getPhotoFilename(): ?string
+    {
+        return $this->photoFilename;
+    }
+
+    public function setPhotoFilename(?string $photoFilename): self
+    {
+        $this->photoFilename = $photoFilename;
+
+        return $this;
+    }
+}
