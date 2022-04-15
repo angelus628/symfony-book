ebfdd14739acdacc9b11a813581e6b011b9143e4
Luis Angel Agudelo Pajoy <angelus628@hotmail.com>
2022-04-15 23:44:04 +0200

    feat: added fixtures to initialize application

---
+++ src/DataFixtures/ConferenceFixtures.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\DataFixtures;
+
+use App\Entity\Comment;
+use App\Entity\Conference;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class ConferenceFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $conference = new Conference();
+        $conference
+            ->setCity('Tokio')
+            ->setYear('1999')
+            ->setIsInternational(true)
+            ->setSlug('-')
+            ;
+        $comment = new Comment();
+        $comment
+            ->setAuthor('Pepito')
+            ->setConference($conference)
+            ->setEmail('pe@pe.pe')
+            ->setText('what an amazing show')
+            ->setPhotoFilename('c57207f7197e.jpg')
+            ;
+        $manager->persist($conference);
+        $manager->persist($comment);
+
+        $conference = new Conference();
+        $conference
+            ->setCity('Bogota')
+            ->setYear('2000')
+            ->setIsInternational(true)
+            ->setSlug('-')
+        ;
+        $manager->persist($conference);
+
+        $conference = new Conference();
+        $conference
+            ->setCity('Madrid')
+            ->setYear('2001')
+            ->setIsInternational(false)
+            ->setSlug('-')
+        ;
+        $manager->persist($conference);
+
+        $manager->flush();
+    }
+}
