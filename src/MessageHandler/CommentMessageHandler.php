807b3cef7c82092246617795490c478ac65b4979
Luis Angel Agudelo Pajoy <angelus628@hotmail.com>
2022-04-23 21:40:29 +0200

    feat: workflow added

--- src/MessageHandler/CommentMessageHandler.php
+++ src/MessageHandler/CommentMessageHandler.php
@@ -7,21 +7,35 @@ namespace App\MessageHandler;
 use App\Message\CommentMessage;
 use App\Repository\CommentRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Psr\Log\LoggerAwareInterface;
+use Psr\Log\LoggerAwareTrait;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
+use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Workflow\WorkflowInterface;
 
 /**
  * Class CommentMessageHandler
  * @package App\MessageHandler
  */
-final class CommentMessageHandler implements MessageHandlerInterface
+final class CommentMessageHandler implements MessageHandlerInterface, LoggerAwareInterface
 {
+    use LoggerAwareTrait;
+
     private EntityManagerInterface $entityManager;
     private CommentRepository $commentRepository;
+    private MessageBusInterface $messageBus;
+    private WorkflowInterface $commentStateMachine;
 
-    public function __construct(EntityManagerInterface $entityManager, CommentRepository $commentRepository)
-    {
+    public function __construct(
+        EntityManagerInterface $entityManager,
+        CommentRepository $commentRepository,
+        MessageBusInterface $messageBus,
+        WorkflowInterface $commentStateMachine
+    ) {
         $this->entityManager = $entityManager;
         $this->commentRepository = $commentRepository;
+        $this->messageBus = $messageBus;
+        $this->commentStateMachine = $commentStateMachine;
     }
 
     public function __invoke(CommentMessage $commentMessage): void
@@ -32,7 +46,29 @@ final class CommentMessageHandler implements MessageHandlerInterface
             return;
         }
 
+        if ($this->commentStateMachine->can($comment, 'accept')) {
+            $score = 0;
+            $transition = 'accept';
+
+            if ($score === 2) {
+                $transition = 'reject_spam';
+            } elseif ($score === 1) {
+                $transition = 'might_be_spam';
+            }
+
+            $this->commentStateMachine->apply($comment, $transition);
+            $this->entityManager->flush();
+            $this->messageBus->dispatch($commentMessage);
+        } elseif ($this->commentStateMachine->can($comment, 'publish') || $this->commentStateMachine->can($comment, 'publish_ham')) {
+            $this->commentStateMachine->apply($comment, $this->commentStateMachine->can($comment, 'publish') ? 'publish' : 'publish_ham');
+            $this->entityManager->flush();
+        } else {
+            $this->logger->debug('Dropping comment message', [
+                'comment' => $comment->getId(),
+                'state' => $comment->getState()
+            ]);
+        }
+
         $comment->setState('published');
-        $this->entityManager->flush();
     }
 }
